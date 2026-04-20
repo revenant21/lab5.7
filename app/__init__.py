5e842255acba481c5abb4d1a3a13856ef6b34373
revenant21 <skyt9755@gmail.com>
2026-04-20 17:58:22 +0600

    Add validation for input amount of money

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,6 @@ def create_app():
 
     # Регистрация модулей
     from .converter.routes import converter_bp
-    app.register_blueprint(converter_bp, url_prefix='/api')
+    app.register_blueprint(converter_bp, url_prefix='/')
 
     return app
